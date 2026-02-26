2bdc25b600ba5e365aab8fad56688e3e9a7de101
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-25 23:58:41 -0700

    save everything

--- reportCardFormatter/Program.cs
+++ reportCardFormatter/Program.cs
@@ -3,33 +3,38 @@ public class Program
 { 
     //global variables
 
-    public static string[] userInput= {0, 0, 0,};
-    public static string[] userGrades= {0, 0, 0,};
+    public static string[] userInput= {"0", "0", "0",};
+    public static int[] userGrades= {0, 0, 0,};
+    public static int userAverage = 0;
 
 	public static void Main()
 	{
 		Console.WriteLine("Please type in 3 grades, 1 at a time, press enter with each number");
         for (int i =0; i>3; i++)
 
-            {
+            {//ask 3 grades and save in array
                 Console.WriteLine("Enter grade");
-                userInput[i] = Console.WriteLine();
-            }
-
-//ask 3 grades and save in array
-
-//end for loop
+                userInput[i] = Console.ReadLine();
+            }//end for loop
 
 
+            Console.WriteLine(userInput[0]); //print 1
+            Console.WriteLine(userInput[1]);//2
+            Console.WriteLine(userInput[2]);//3
 
 
-//print grades they gave using {userInput[0]} 0, 1, and 2
+
 Console.WriteLine($" The grades you have entered are {userInput[1]}, {userInput[2]}, {userInput[3]},");
 
+
+// I had an idea to do this in swoop and the internet said yes use this method 
+// Converts the entire array in one line
+userGrades = Array.ConvertAll(userInput, int.Parse);
 //do average
+userAverage = (userInput[1] + userInput[2] + userInput[3])/3;
 
-// console write line print average
-
+Console.WriteLine($" Your grade average based on this total is {userAverage}");
+    
 	}
 }
 
