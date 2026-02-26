edf975af3f238a5d39a1988ec461630eb52e8c47
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-26 00:14:16 -0700

    fixed some bugs where i didnt start at index 0 for printing some arrays, other bugs like less than verses greater than sign

--- reportCardFormatter/Program.cs
+++ reportCardFormatter/Program.cs
@@ -7,34 +7,34 @@ public class Program
     public static int[] userGrades= {0, 0, 0,};
     public static int userAverage = 0;
 
-	public static void Main()
-	{
-		Console.WriteLine("Please type in 3 grades, 1 at a time, press enter with each number");
-        for (int i =0; i>3; i++)
+    public static string userName = "john-jane";
 
+	public static void Main()
+	{   
+		Console.WriteLine("Hi there, whats your name?");
+		userName = Console.ReadLine();
+        Console.WriteLine("Please type in 3 grades, 1 at a time, press enter with each number");
+        for (int i =0; i<3; i++)
             {//ask 3 grades and save in array
                 Console.WriteLine("Enter grade");
                 userInput[i] = Console.ReadLine();
             }//end for loop
 
+        Console.WriteLine(userInput[0]); //print 1
+        Console.WriteLine(userInput[1]);//2
+        Console.WriteLine(userInput[2]);//3
 
-            Console.WriteLine(userInput[0]); //print 1
-            Console.WriteLine(userInput[1]);//2
-            Console.WriteLine(userInput[2]);//3
-
-
-
-Console.WriteLine($" The grades you have entered are {userInput[1]}, {userInput[2]}, {userInput[3]},");
+        Console.WriteLine($" The grades you have entered are {userInput[0]}, {userInput[1]}, {userInput[2]},");
 
 
-// I had an idea to do this in swoop and the internet said yes use this method 
-// Converts the entire array in one line
-userGrades = Array.ConvertAll(userInput, int.Parse);
-//do average
-userAverage = (userInput[1] + userInput[2] + userInput[3])/3;
+        // I had an idea to do this in swoop and the internet said yes use this method 
+        // Converts the entire array in one line
+        userGrades = Array.ConvertAll(userInput, int.Parse);
+        //do average
+        userAverage = (userGrades[0] + userGrades[1] + userGrades[2])/3;
 
-Console.WriteLine($" Your grade average based on this total is {userAverage}");
-    
-	}
+        Console.WriteLine($" {userName} your grade average based on this total is {userAverage}");
+            
+	    }
 }
 
